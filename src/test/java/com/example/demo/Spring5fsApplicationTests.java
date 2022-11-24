449b54820f56c2ad8a50fb387a9cbbd52fa5451f
namyun1012 <keukki2@gmail.com>
2022-11-24 15:48:43 +0900

    12

---
+++ src/test/java/com/example/demo/Spring5fsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5fsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
